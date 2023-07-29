a8371ea71d729fc930becf4f7159387472772dba
tvp <kevinphong1203@gmail.com>
2023-07-29 14:35:44 +0700

    add repository and service

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Category;
+
+class CategoryRepository extends BaseRepository
+{
+     private $model;
+
+     public function __construct(Category $model)
+     {
+         $this->model = $model;
+         parent::__construct($model);
+     }
+
+    public function getCategories($page)
+     {
+        return $this->model->with(['parentCategory'])->paginate(10, '*','p',$page);
+     }
+
+}
---
+++ app/Services/CategoryService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class CategoryService
+{
+   private $model;
+
+   public function __construct($model)
+   {
+       $this->model = $model;
+   }
+
+    public function getById($id)
+    {
+       return $this->model->find($id);
+    }
+
+    public function create($attribute = [])
+    {
+       return $this->model->create($attribute);
+    }
+}
